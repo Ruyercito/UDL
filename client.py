a9453e944e4dc86b487bacae47c943e01f93e434
Ruyercito <56358111+Ruyercito@users.noreply.github.com>
2020-03-10 04:07:34 -0700

    Update client.py

--- client.py
+++ client.py
@@ -44,10 +44,8 @@ p = 3
 q = 3
 break_loop = 0
 
-def print_debug(message):
-	if strcmp(argv[i], "-d") == 0:
-		print_time()
-		printf(message)
+debug_mode = false
+network_dev_config_file_name = NULL
 
 def print_time():
 	time(timer)
@@ -56,6 +54,7 @@ def print_time():
 	strftime(buffer, 26, "%H: %M: %S", tm_info)
 	printf(buffer)
 
+
 def read_client_configuration(file):
 	for i in range(0, 13):
 		package_id[i] = '\0'
@@ -63,33 +62,79 @@ def read_client_configuration(file):
 	for i in range()
 
 
-
 def send_udp_message(package):
 	if sendto(sock_udp, package, 84, 0, server_addr, sizeof(server_addr)) < 0:
-		print_debug("Sending error\n")
+		print("Sending error\n")
 	if package[0] == REG_REQ:
-		print_debug("REG_REQ has been sent\n")
+		print("REG_REQ has been sent\n")
 	elif package[0] == ALIVE:
-		print_debug("ALIVE package has been sent\n")
+		print("ALIVE package has been sent\n")
 	else:
-		print_debug("Bad type of package has been sent\n")
+		print("Bad type of package has been sent\n")
+
 
 def create_package(package_type, data):
 	result = malloc(84)
-	result[0] =  package_type
+	result[0] = package_type
 
 	for i in range(0, 13):
-		result[i+1] = package_id[i]
+		result[i + 1] = package_id[i]
 	for i in range(0, 9):
-		result[i+14] = random_number[i]
+		result[i + 14] = random_number[i]
 	for i in range(0, 61):
-		result[i+23] = data[i]
+		result[i + 23] = data[i]
 
 	return result
 
+
+def parse_argv(argc, argv):
+	software_config_file = NULL
+	for i in range(0, argc):
+		if strcmp(argv[i], "-c") == 0 & aargc > (i + 1):
+			if access(argv[i + 1], F_OK) != -1:
+				software_config_file = fopen(argv[i + 1], "r")
+			else:
+				sprintf(message, "ERROR: Can't open the file " + argv[i + 1] +". Will open client.cfg (default config.file)\n")
+				print(message)
+
+		elif strcmp(argv[i], "-d") == 0:
+			debug_mode = true
+			print("INFO: Debug mode activated (-d)\n")
+		elif strcmp(argv[i], "-f") == 0 & argc > (i + 1):
+			network_dev_config_file_name = malloc(sizeof(argv[i + 1]))
+			strcpy(network_dev_config_file_name, argv[i + 1])
+
+	if debug_mode: print("DEBUG: Read command line input\n")
+	if software_config_file == NULL:
+		if access("client.cfg", F_OK) != -1:
+			software_config_file = fopen("client.cfg", "r")
+		else:
+			printf("ERROR: Can't find default file named client.cfg in current directory\n ")
+			exit(1)
+	if network_dev_config_file_name == NULL:
+		network_dev_config_file_name == malloc(sizeof("boot.cfg"))
+		strcpy(network_dev_config_file_name, "boot.cfg")
+	save_software_config_file_data(software_config_file)
+	if debug_mode: printf("DEBUG: Read data from configuration files\n")
+
+def save_software_config_file_data(software_config_file):
+	while fgets(line, 70, software_config_file):
+		token = strtok(line, "\n")
+
+		if strcmp(token, "Id") == 0:
+			token = strtok(NULL, "\n")
+			strcpy(client_data.package_id, token)
+		elif strcmp(token, "Server") == 0:
+			token = strtok(NULL, "\n")
+			server_data.address = malloc(strlen(token) + 1)
+			strcpy(server_data.address, token)
+		elif strcmp(token, "Server-port") == 0:
+			sockets.udp_port = atoi(strtok(NULL, "\n"))
+
+
 def register_attempt():
-	for j in range(0, 8) & (status == WAIT_ACK_REG | status == DISCONNECTED) & break_loop == 0
-		if j < 1 :
+	for j in range(0, 8) & (status == WAIT_ACK_REG | status == DISCONNECTED) & break_loop == 0:
+		if j < 1:
 			send_udp_message(create_package(REG_REQ, ""))
 			if status == DISCONNECTED:
 				status = WAIT_ACK_REG
